522020eb053659465451291e86fdd273e81f4fad
Milica Aleksic <milica.aleksic@pmf.edu.rs>
2017-09-20 16:38:27 +0200

    fi

--- map.js
+++ map.js
@@ -1,9 +1,23 @@
+var myCenter = new google.maps.LatLng(45.112629, 14.850351);
 
-function myMap() {
-var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-var marker = new google.maps.Marker({position:myCenter});
+function initialize() {
+var mapProp = {
+  center:myCenter,
+  zoom:14,
+  mapTypeId:google.maps.MapTypeId.ROADMAP};
+var map = new google.maps.Map(document.getElementById("map"),mapProp);
+var marker = new google.maps.Marker({position:myCenter,});
 marker.setMap(map);
+/*info window on click*/
+var infowindow = new google.maps.InfoWindow({
+  content:"Vila Daca"
+  });
+
+
+google.maps.event.addListener(marker, 'click', function () {
+    map.panTo(marker.getPosition());
+    infowindow.open(map,marker);
+});
 }
 
+google.maps.event.addDomListener(window, 'load', initialize);
